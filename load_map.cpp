615edc4b6ce8e06bebf256b5b7cac3917674c15d
Joseph May <joseph.may@S-MAY013.local>
2014-03-09 06:10:56 -0400

    added first file

---
+++ load_map.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+class RoomMap {
+  void load_map (const std::string&);
+};
+
+
+void RoomMap::load_map (const std::string& fileName)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  if (pcl::io::loadPCDFfile<pcl::PointXYZ> (fileName, *cloud) == -1) //* loads the file
+    {
+      PCL_ERROR ("Couldn't read map file \n");
+      return (-1);
+    }
+    std::cout << "Loaded "
+              << cloud->width * cloud->height
+              <<" data points from map.pcd with the following fields: "
+              << std::endl;
+    for (size_t i =0; i< cloud->points.size (); ++i)
+      std::cout << "  "<< cloud->points[i].x
+                << " " << cloud->points[i].y
+                << " " << cloud->points[i].z <<std::endl;
+}
+
+int
+main ()
+  {
+      RoomMap livingRoom;
+      livingRoom.load_map ("map.pcb");
+      return (0);
+  }
